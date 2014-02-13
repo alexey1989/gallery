eaab7d68d23369daa7a9de7a88d9d8e2e02c77ce
alexey1989 <alexey.gorbashko@gmail.com>
2014-02-13 17:31:11 +0200

    add new project with pg

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Gallery::Application.config.secret_key_base = '98629f5f711ec6a8d546a0ebdff152cd661b3527c41d8a1f774dca4d437fe0c6ca0e494699ace2140660211585cc22bf41b2338e85bfcab9e9360a84e3668d8c'
